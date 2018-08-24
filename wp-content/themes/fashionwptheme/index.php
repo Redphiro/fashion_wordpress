032ac094acd7ce283edb3e62673f85a6e49116ba
Redphiro <sebastian.alvcort@gmail.com>
2018-08-24 20:34:50 -0300

    Creamos el Loop de Noticias

--- wp-content/themes/fashionwptheme/index.php
+++ wp-content/themes/fashionwptheme/index.php
@@ -17,7 +17,7 @@
         
         	<h3>Lo ultimo de nuestro blog</h3>
             <div class="contenedor">
-            <article>
+            <!--<article>
             	<img src="<?php echo get_bloginfo('template_url'); ?>/img/articulo-1.jpg">
             	<h4>Articulo 1</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -33,7 +33,28 @@
             	<img src="<?php echo get_bloginfo('template_url'); ?>/img/articulo-3.jpg">
             	<h4>Articulo 3</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </article>-->
+            
+            <?php
+               global $paged;
+			   $my_query_current = $paged ? $paged : 1;
+			   $my_query_args = array('paged' => $my_query_current,
+			                          'post_type' => 'noticias',
+									  'tipo_noticias' => $_GET['tipo'],
+									  );
+				$my_query = new WP_Query($my_query_args);
+				while ($my_query->have_posts()) : $my_query->the_post(); $do_not_duplicate = $post->ID; 					  
+            
+            ?>
+            <article>
+            	<img src="<?php echo imageFeatured($post->ID,'medium'); //tamaños son: thumbnail, medium, large, full ?>">
+                <h4><?php echo get_the_title(); ?></h4>
+                <p><?php echo get_the_excerpt(); ?></p>
+            
             </article>
+               <?php 
+				endwhile; 
+			  ?>
             </div>
         </section>
         <section id="info">
